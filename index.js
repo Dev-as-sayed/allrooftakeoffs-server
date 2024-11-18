7cebc46b8e36ed6c84b81e97c4fc43687b060cab
Dev-as-sayed <assayed277@gmail.com>
2024-11-18 18:06:49 +0600

    add origin on this commit

--- index.js
+++ index.js
@@ -22,20 +22,13 @@ const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 app.use(cors());
-// midleWare
-// app.use(
-//   cors({
-//     origin: [
-//       "https://allrooftakeoffs-client.vercel.app",
-//       "http://localhost:5173",
-//     ],
-//     credentials: true,
-//   })
-// );
+
 
 const allowedOrigins = [
-  "https://allrooftakeoffs-client.vercel.app",
-  "http://localhost:5173",
+  // "https://allrooftakeoffs-client.vercel.app",
+  "http://www.allrooftakeoffs.com.au"
+  "http://allrooftakeoffs.com.au"
+  // "http://localhost:5173",
 ];
 
 app.use(
